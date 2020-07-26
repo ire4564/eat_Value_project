20a5fae6e5b85e2cac6c86b9ac119dde56f69908
Begachu <ygo65312@naver.com>
2020-07-27 01:56:29 +0900

    20200727 Begachu 모드별 화면들이 유저 정보를 받게 수정

--- views/now_order.js
+++ views/now_order.js
@@ -13,6 +13,12 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator, TextInput } from 'react-native';
 
 class NowOrder extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            user: this.props.user
+        }   
+    }
     render(){
         return(
             <View style={this.props.style}>
--- views/talk.js
+++ views/talk.js
@@ -13,6 +13,12 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator, TextInput } from 'react-native';
 
 class Talk extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            user: this.props.user
+        }   
+    }
     render(){
         return(
             <View style={this.props.style}>
