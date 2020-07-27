ddf6422c2d7d8b6f963ec3a6ee8d076d3e6adcf3
Begachu <ygo65312@naver.com>
2020-07-28 03:27:23 +0900

    20200728 Begachu 주문목록용 요소 완성

--- components/touchableList.js
+++ components/touchableList.js
@@ -2,7 +2,9 @@
  * Class : 클릭 가능한 세부 주문 목록 요소 컴포넌트
  * 
  * props :
- *  - text : 버튼에 표기할 문자
+ *  - order: 주문 정보
+ *  - store: 가게 정보
+ *  - style
  * 
  * function :
  *  -  
@@ -10,49 +12,114 @@
  ************************************************/
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import TouchableText from './TouchableText';
 
-
-class TouchableText extends Component {
+class TouchableList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            db_order: this.props.order,
-            style: styles.container,
-            style_text: styles.text,
+            order: this.props.order,
+            store: this.props.store,
         }
+    }
+    computeGauge(){
+        var i = this.state.order.current_order/this.state.order.limit_order;
+        i = i * 100;
+        return i+"%";
     }
 
     render(){
         return(
             <TouchableOpacity
-            style={this.state.style}
+            style={styles.container}
             onPress={function(){
                 if(this.props.event!==undefined){
                     this.props.event;
                 }
             }.bind(this)}
             >
-                <Text style={this.state.style_text}>{this.props.text}</Text>
+                <TouchableText
+                text={this.state.store.category}
+                style={styles.category_container}
+                style_text={styles.category_text}/>
+                <Text style={styles.text}>{this.state.store.name}</Text>
+
+                <View style={styles.gaugeBar}>
+                    <View style={[styles.gauge, {width: this.computeGauge()}]}/>
+                </View>
+                <Text style={styles.number}>{this.state.order.current_order}/{this.state.order.limit_order}</Text>
             </TouchableOpacity>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    //전체 컨테이너 style
     container: {
+        marginHorizontal: wp('5%'),
+        marginVertical: hp('0.5'),
+        paddingHorizontal: wp('1%'),
+        paddingVertical: hp('1.5%'),
+        borderBottomWidth: hp('0.3%'),
+        borderColor: '#ddd',
+        flexDirection: 'row',
+    },
+
+    //카테고리 상자 style
+    category_container: {
         backgroundColor: '#999',
         borderRadius: 25,
-        marginRight: 6,
+        alignContent: 'center',
+        justifyContent: 'center',
+        height: hp('2%'),
+        width: wp('15%'),
+        alignSelf: 'center',
     },
-    text: {
-        width: 100,
-        height: 25,
+
+    //카테고리 문자 style
+    category_text: {
+        fontSize: hp('1.2%'),
         color: '#fff',
-        fontSize: 13,
         textAlign: 'center',
         textAlignVertical: 'center',
-    }
+        
+    },
+
+    //가게명 style
+    text: {
+        width: wp('40%'),
+        marginLeft: wp('2%'),
+        fontSize: hp('2%'),
+        textAlignVertical: 'center',
+    },
+
+    //인원 수 style
+    number: {
+        width: wp('10%'),
+        marginLeft: wp('2%'),
+        fontSize: hp('2%'),
+        textAlignVertical: 'center',
+    },
+
+    //게이지바 style
+    gaugeBar: {
+        width: wp('20%'),
+        height: hp('2.5%'),
+        borderColor: '#aaa',
+        borderWidth: hp('0.2%'),
+        borderRadius: 25,
+        alignContent: 'center',
+        alignSelf: 'center',
+    },
+
+    //게이지 style
+    gauge: {
+        height: hp('2.1%'),
+        borderRadius: 25,
+        backgroundColor: '#00CED1',
+    },
   });
 
-export default TouchableText;
+export default TouchableList;
