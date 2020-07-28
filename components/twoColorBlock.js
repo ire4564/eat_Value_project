460cfd57c96a705316f8b7a275b3f85af164441e
Begachu <ygo65312@naver.com>
2020-07-28 21:01:22 +0900

    20200728 Begachu 2분할 컴포넌트 추가

---
+++ components/twoColorBlock.js
@@ -0,0 +1,76 @@
+/************************************************
+ * Class : 가게 사진이 포함된 버튼 컴포넌트
+ * 
+ * props :
+ *  - height: 전체 컴포넌트 크기
+ *  - topHeight: 상위 블록 부분 비율
+ *  - bottomHeight: 하위 블록 부분 비율
+ *  - top: 상위 블록에 넣어야하는 컴포넌트
+ *  - bottom: 하위 블록에 넣어야하는 컴포넌트
+ * 
+ * state :
+ *  - 
+ * 
+ * function :
+ *  - topHeight: 상단 블록 높이 설정
+ *  - bottomHeight: 하단 블록 높이 설정
+ *  
+ ************************************************/
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+class TwoColorBlock extends Component {
+    topHeight(){
+        var temp = (this.props.topHeight/(this.props.topHeight+this.props.bottomHeight))*100;
+        return temp+"%";
+    }
+    bottomHeight(){
+        var temp =(this.props.bottomHeight/(this.props.topHeight+this.props.bottomHeight))*100;
+        return temp+"%";
+    }
+    render(){
+        return(
+            <View style={[styles.container,{height: this.props.height}]}>
+                <View style={[styles.top_container, {height: this.topHeight()}]}>
+                    {this.props.top}
+                </View>
+                <View style={[styles.bottom_container, {height: this.bottomHeight()}]}>
+                    {this.props.bottom}
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        borderTopWidth: 1,
+        borderBottomWidth: 4,
+        borderLeftWidth: 1,
+        borderRightWidth: 4,
+        borderRadius: 25,
+        borderColor: '#ccc',
+        alignContent: 'space-around',
+    },
+    top_container: {
+        width: '100%',
+        borderTopLeftRadius: 22,
+        borderTopRightRadius: 22,
+        backgroundColor: '#fff',
+        alignContent: 'center',
+        justifyContent: 'center',
+    },
+    bottom_container: {
+        width: '100%',
+        borderBottomLeftRadius: 21,
+        borderBottomRightRadius: 21,
+        backgroundColor: '#eee',
+        alignContent: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default TwoColorBlock;
