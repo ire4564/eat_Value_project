229da9fa714c94e26fc3358fe3ce56f155e58545
Begachu <ygo65312@naver.com>
2020-08-03 19:16:11 +0900

    20200803 Begachu 채팅창 출력메소드 추가 및 사용

--- views/talk.js
+++ views/talk.js
@@ -1,11 +1,19 @@
 /************************************************
- * Class : 
+ * Class : 채팅 화면
  * 
  * state :
- *  - 
+ *  - db_user: 유저 정보
+ * //여기 아래부터는 db 변동에 따라 수정이 필요함
+ *  - test_users: 전체 유저 정보
+ *  - talk: 유저의 채팅 목록
+ *      - user: 채팅한 유저의 유저 num
+ *      - detail: 채팅 내역
+ *          - text: 채팅 내용
+ *          - user: 해당 채팅을 말한 유저 num
+ *          - date: 보낸 시간 
  * 
  * function :
- *  -  
+ *  - chattingList: 채팅방 출력 메소드
  *  
  ************************************************/
 
@@ -20,9 +28,63 @@ class Talk extends Component {
     constructor(props){
         super(props);
         this.state = {
-            db_user: this.props.db_user
+            db_user: this.props.db_user,
+            //이 아래부터는 db적용을 위해 수정이 필요한 state임!!
+            test_users: [
+                {   coupon_num: 0,
+                    id: "testID",
+                    name: "테스트계정",
+                    order_num: 0,
+                    location: "대전 유성구 궁동 99",
+                  },
+                {   coupon_num: 0,
+                    id: "testID2",
+                    name: "비가츄",
+                    order_num: 0,
+                    location: "대전 유성구 궁동 99",
+                  },
+                {   coupon_num: 1,
+                    id: "testID3",
+                    name: "바지",
+                    order_num: 0,
+                    location: "대전 유성구 궁동 99",
+                  },
+                {   coupon_num: 2,
+                    id: "testID4",
+                    name: "먹꼬쟈",
+                    order_num: 0,
+                    location: "대전 유성구 궁동 99",
+                  },
+            ],
+            talk: [
+                {user: 1,
+                    detail:[
+                        {text: "안녕하세요", user:0, date:"2020-08-01 09:20" },
+                        {text: "안녕하세요!", user:1, date:"2020-08-01 09:23" },
+                        {text: "오늘 같이 시켜먹어요!!", user:1, date:"2020-08-01 13:51" },
+                    ]
+                },
+            ],
         }   
     }
+
+    chattingList(){
+        var list = [];
+        for(let i=0; i<this.state.talk.length; i++){
+            var talk = this.state.talk[i];
+            var user = this.state.test_users[talk.user];
+            list.push(<TouchableOpacity style={styles.talk_container} key={i+"_talk"}>
+                    <Image source={require(UNNAMED)} style={styles.profile_img}/>
+                    <View style={styles.talk_text_container}>
+                        <Text style={[{fontWeight: 'bold'}, styles.talk_text]} numberOfLines={1}>{user.name}</Text>
+                        <Text style={styles.talk_text} numberOfLines={1}>{talk.detail[talk.detail.length-1].text}</Text>
+                    </View>
+                    <Text style={styles.talk_date}>{talk.detail[talk.detail.length-1].date}</Text>
+                </TouchableOpacity>);
+        }
+        return list;
+    }
+
     render(){
         return(
             <View style={this.props.style}>
@@ -32,14 +94,7 @@ class Talk extends Component {
                         <Text style={styles.header_text}><Text style={{fontWeight: 'bold'}}> 같이먹기</Text>를 제안해보세요!</Text>
                     </View>
                     <ScrollView style={styles.talk_scroll}>
-                        <TouchableOpacity style={styles.talk_container}>
-                            <Image source={require(UNNAMED)} style={styles.profile_img}/>
-                            <View style={styles.talk_text_container}>
-                                <Text style={[{fontWeight: 'bold'}, styles.talk_text]} numberOfLines={1}>비가츄</Text>
-                                <Text style={styles.talk_text} numberOfLines={1}>오늘 같이 시켜먹어요!</Text>
-                            </View>
-                            <Text style={styles.talk_date}>2020-08-03</Text>
-                        </TouchableOpacity>
+                        {this.chattingList()}
                     </ScrollView>
                     <TouchableOpacity style={styles.button_container}>
                     <MaterialCommunityIcons name="message-plus" size={hp('3%')} color="#fff" />
