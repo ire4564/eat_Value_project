838b04e95edd1e9ab688fe2514899c4175200ab4
Begachu <ygo65312@naver.com>
2020-08-14 04:26:46 +0900

    20200814 Begachu 상세 주문 배달 위치까지 디자인 완성

--- views/detail_order.js
+++ views/detail_order.js
@@ -14,6 +14,7 @@ import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import moment from 'moment'; 
 import posed from 'react-native-pose';
+import { AntDesign } from '@expo/vector-icons'; 
 
 const TEST_IMG = '../images/detail_order_sample.png';
 const COLOR_SET = ['#00CED1','#8BAAF0', '#7AD3FA', '#40e0d0'];
@@ -68,14 +69,26 @@ class DetailOrder extends Component {
                         <View>
                             <Text style={styles.store_name}>동대문 엽기 떡볶이</Text>
                             <Text style={styles.store_info}>대전광역시 유성구 봉명동</Text>
-                            <Text style={styles.eat_with_box}>같이 먹어요</Text>
+                            <View style={styles.eat_with_box}>
+                                <Text style={styles.eat_with_box_text}>같이 먹어요 </Text>
+                                <AntDesign name="questioncircleo" style={{textAlignVertical: 'center'}} size={wp('3.5%')} color="#000" />
+                            </View>
                         </View>
                         
                         <View style={styles.order_container}>
-                            <Text></Text>
-                            <Text>대전광역시 유성구 궁동 99 조각공원</Text>
-                            <Text>인원 제한</Text>
-                            <Text>상세 주문 내역</Text> 
+                            <View style={styles.map_container}>
+                                <View style={styles.mini_map}/>
+                                <View style={{width: wp('48%')}}>
+                                    <Text style={styles.order_header}>배달 위치</Text>
+                                    <Text style={styles.order_location}>대전광역시 유성구 궁동 99 조각공원</Text>
+                                    <Text style={{color: '#aaa', fontSize: wp('3%')}}>
+                                    ※ '혼자 먹어요'의 경우 해당 위치는 전체 배달의 기준점이 되는 위치를 의미합니다.</Text>
+                                </View>
+                            </View>
+                            
+                            <Text style={styles.order_header}>앞으로 2명 더 모이면 자동주문!</Text>
+                            <Text style={styles.order_header}>최소 결제 금액 달성까지 앞으로</Text>
+                            <Text style={styles.order_header}>상세 주문 내역</Text> 
                         </View>
                           
                     </View>
@@ -129,7 +142,7 @@ const styles = StyleSheet.create({
       store_name: {
         marginTop: hp('1%'),
         fontWeight: 'bold',
-        fontSize: wp('7%'),
+        fontSize: wp('6.5%'),
       },
 
       //같이/혼자 먹어요 상자 style
@@ -137,26 +150,56 @@ const styles = StyleSheet.create({
         marginTop: hp('1%'),
         position: 'absolute',
         alignSelf: 'flex-end',
-        fontSize: wp('4%'),
-        fontWeight: 'bold',
+        flexDirection: 'row',
         backgroundColor: COLOR_SET[0],
         paddingHorizontal: wp('3%'),
         paddingVertical: wp('2%'),
         borderRadius: wp('2%'),
       },
+      eat_with_box_text: {
+        fontSize: wp('4%'),
+        fontWeight: 'bold',
+      },
 
       //가게 정보 text style
       store_info: {
-
+          fontSize: wp('3.6%'),
       },
 
       //주문 관련 text container style
       order_container:{
-          marginVertical: hp('2%'),
+          marginVertical: hp('2.5%'),
+          paddingTop: hp('2%'),
           borderTopWidth: 2,
           borderColor: '#ddd',
       },
 
+      //모이는 주소가 나와있는 컴포넌트 style
+      map_container: {
+          flexDirection: 'row',
+          marginBottom: hp('2%'),
+      },
+
+      //작은 지도 style
+      mini_map: {
+          backgroundColor: '#ddd',
+          width: wp('40%'),
+          height: wp('30%'),
+          marginRight: wp('2%'),
+      },
+
+      //주소 text style
+      order_location: {
+          fontSize: wp('4%'),
+          marginBottom: wp('3%'),
+      },
+
+      //주문 내역 내용 중 각 헤더 text
+      order_header: {
+        fontWeight: 'bold',
+        fontSize: wp('5%'),
+        marginBottom: wp('1%'),
+      },
   });
 
 export default DetailOrder;
