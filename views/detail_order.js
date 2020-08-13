2ce56152d59da9435faca836992722d39310162c
Begachu <ygo65312@naver.com>
2020-08-13 22:27:46 +0900

    20200813 Begachu 오타 수정 및 애니메이션 추가

--- views/detail_order.js
+++ views/detail_order.js
@@ -10,19 +10,64 @@
  ************************************************/
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import posed from 'react-native-pose';
+
+const Page = posed.View({
+    open: {
+        y: 0,
+        opacity: 1,
+        transition: {
+          y: { 
+              type: 'spring', 
+              stiffness: 500, 
+              damping: 100
+            },
+        }
+    },
+    closed: {
+        y: hp('5%'), 
+        opacity: 0
+    },
+});
 
 class DetailOrder extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: this.props.data,
+            db_user: this.props.db_user,
+            db_store: this.props.db_store,
+            db_order: this.props.db_order,
+            event: 'closed',
+        }
+    }
+    componentDidMount() {
+        this.setState({event: 'open'});
+    }
+
     render(){
         return(
-            <View style={this.props.style}>
-                <Text>{this.props.data}</Text>
-            </View>
+            <Page style={this.props.style} pose={this.state.event}>
+                <View style={styles.main_container}>
+                </View>
+            </Page>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    //메인 컨테이너 style
+    main_container: {
+        position: 'absolute',
+        width: wp('100%'),
+        height: hp('85%'),
+        top: hp('-3%'),
+        paddingHorizontal: wp('5%'),
+        paddingVertical: hp('3%'),
+        backgroundColor: '#fff',
+      },
   });
 
 export default DetailOrder;
--- views/make_room.js
+++ views/make_room.js
@@ -16,7 +16,7 @@
 
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView, TextInput, CheckBox } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { Divider } from 'react-native-elements';
 import LocationBar from '../components/locationBar';
